7ee7fb8cdf810fb95b4902a038576fda74d45a7d
Anders Bergman <anders.bergman@physics.uu.se>
2022-10-25 12:21:10 +0200

    Updated more cleaning scripts.

--- tests/cleanAll.sh
+++ tests/cleanAll.sh
@@ -2,9 +2,9 @@
 
 # Deletes all .out files.
 # Add more cleanup fileformates as needed
-find . -name '*.*.yaml' -type f -delete
-find . -name '*.json' -type f -delete
-find . -name '*.out' -type f -delete
-find . -name 'meminfo' -type f -delete
-find . -name 'snap*.p??' -type f -delete
-find . -name 'fort.*' -type f -delete
+find */. -name '*.*.yaml' -type f -delete
+find */. -name '*.json' -type f -delete
+find */. -name '*.out' -type f -delete
+find */. -name 'meminfo' -type f -delete
+find */. -name 'snap*.p??' -type f -delete
+find */. -name 'fort.*' -type f -delete
