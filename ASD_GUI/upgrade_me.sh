498db9fa0cac2576af45a1232aa990aae4591d40
Anders Bergman <anders.bergman@physics.uu.se>
2022-12-05 17:06:52 +0100

    Added scripts to modify the Qt version needed for the GUI.

---
+++ ASD_GUI/upgrade_me.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# upgrade_me.sh
+echo 'This script will restore the Qt requirements from Qt5 to Qt6 for the ASD_GUI'
+echo 'Use this as a last resort, a better approach is to ask your system administrator'
+echo 'to install an up-to-date Python environment.'
+
+for i in setup.py bin/asd_gui `find . -name "*.py"` ; 
+do 
+   sed -i  "s/Qt5/Qt6/g" $i 
+done
+echo '-----------------------------------------------'
+
+echo 'Downgrade complete.'
+echo '-----------------------------------------------'
+echo 'Install the GUI as follows:'
+
+echo '> python -m pip install virtualenv'
+echo '> python -m virtualenv ASD_GUI_env'
+echo '> source ASD_GUI_env/bin/activate'
+echo '> pip install numpy matplotlib pyyaml pandas pyqt6 vtk'
+echo '> pip install . ' 
+echo '-----------------------------------------------'
+echo 'If successful, you can then invoke the GUI with' 
+echo '> asd_gui'
+echo 'If not, please consider reporting this at https://github.com/UppASD/UppASD/issues'
