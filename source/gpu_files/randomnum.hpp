35af54cb07ee728c9ff191cc2bd6ac3d744807e0
Arkadijs Slobodkins <aslobodkins@smu.edu>
2024-03-12 15:36:44 +0100

    added std::size_t

--- source/gpu_files/randomnum.hpp
+++ source/gpu_files/randomnum.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
 #include "real_type.h"
+#include <cstdio>
 
 class RandomNumbers {
 public:
    RandomNumbers();
    ~RandomNumbers();
 
-   void fillArray(real* v, size_t len);
+   void fillArray(real* v, std::size_t len);
 };
 
 
