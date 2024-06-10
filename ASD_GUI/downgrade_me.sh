2815a0093edc9d29cab0413c4c58a7564ab88704
Anders Bergman <anders.bergman@physics.uu.se>
2024-06-10 16:50:26 +0200

    Testing wheel building for the GUI IV

--- ASD_GUI/downgrade_me.sh
+++ ASD_GUI/downgrade_me.sh
@@ -8,6 +8,7 @@ for i in setup.py bin/asd_gui `find . -name "*.py"` ;
 do 
    sed -i  "s/Qt6/Qt5/g" $i 
 done
+sed -i "s/asd_gui/asd_gui_qt5/g" setup.py
 echo '-----------------------------------------------'
 
 echo 'Downgrade complete.'
@@ -21,5 +22,5 @@ echo '> pip install numpy matplotlib pyyaml pandas pyqt5 vtk'
 echo '> pip install . ' 
 echo '-----------------------------------------------'
 echo 'If successful, you can then invoke the GUI with' 
-echo '> asd_gui'
+echo '> asd_gui_qt5'
 echo 'If not, please consider reporting this at https://github.com/UppASD/UppASD/issues'
