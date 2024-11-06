9f73461e0c0dd77873bb734f7db7ebd6a290f721
Johan Hellsvik <hellsvik@kth.se>
2024-11-06 09:57:13 +0100

    Added prn_cudameasurements.f90

---
+++ source/Measurement/prn_cudameasurements.f90
@@ -0,0 +1,46 @@
+!------------------------------------------------------------------------------------
+! MODULE: prn_cudameasurements
+!> @brief Data structures and subroutines for printing measurements calculated in CUDA
+!> @author Johan Hellsvik
+!> @copyright
+!> GNU Public License
+!------------------------------------------------------------------------------------
+module prn_cudameasurements
+
+   use Parameters
+   use ErrorHandling
+
+   implicit none
+
+   ! Printing definitions
+
+   ! Local scalars
+
+   ! Local arrays
+
+   ! Private variables
+   private
+
+   ! Public variables
+
+contains
+
+   !---------------------------------------------------------------------------------
+   !> @brief
+   !> Wrapper for printing measurements calculated in CUDA
+   !
+   !> @author Johan Hellsvik
+   !---------------------------------------------------------------------------------
+   subroutine print_measurements()
+
+     implicit none
+
+     ! Printing definitions
+
+     ! Local scalars
+
+     ! Local arrays
+
+   end subroutine print_measurements
+
+end module prn_cudameasurements
