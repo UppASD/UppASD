fba84eca39818bc32f123da4081ee73a4e84731e
Anders Bergman <anders.bergman@physics.uu.se>
2026-01-31 12:56:55 +0100

    Exposed notebook methods in run/__init__.py

--- uppasd/run/__init__.py
+++ uppasd/run/__init__.py
@@ -1,8 +1,11 @@
 from .simulator import ASDWorkspace, UppASDSimulator
 from .live_simulator import LiveSimulator
+from .notebook_viewer import NotebookLiveViewer, ViewerConfig
 
 __all__ = [
     "ASDWorkspace",
     "UppASDSimulator",
     "LiveSimulator",
+    "NotebookLiveViewer",
+    "ViewerConfig",
 ]
